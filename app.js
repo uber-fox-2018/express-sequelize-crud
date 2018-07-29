180f2feab74854a6f1a834d2dcfd3fb3849be085
harlesbayu <bayuanggaraharles@gmail.com>
2018-07-29 21:00:43 +0700

    routes sudah dirapihan, crud student sudah selesai tinggal merapihkan html dan css

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
 const express = require('express')
 const app     = express()
+const routes = require('./routes')
+
 const port    = 3000
 const Model   = require('./models')
 
 app.use(express.static(__dirname + '/views'))
 app.set('view engine', 'ejs')
 
-app.get('/students', (req,res) => {
-    Model.Student.findAll({raw:true})
-        .then( element => {
-            res.render('student.ejs', { dataStudent : (element)})
-        })
-})
+app.use('/', routes)
 
-app.get('/add-students', (req,res) => {
-    res.render()
-})
-
-app.get('/teachers', (req,res) => {
-    res.send('ini halaman teacher')
-})
-
-app.get('/', (req,res) => {
-    res.send('ini halaman home')
-})
 
 app.listen(port, () => {
     console.log('app listening on port ', port)
